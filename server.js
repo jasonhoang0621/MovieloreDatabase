c52c603ebc6c8150e6a96ea12a71a1229d4c1325
Jason Hoang <jasonhoang6201@gmail.com>
2021-12-08 12:14:36 +0700

    add corse

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors')
+
 const app = express();
+
 const route = require('./Routes');
 const port = process.env.PORT || 3000;
+app.use(cors());
 
 const mongoose = require('mongoose');
 
